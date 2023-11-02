b9e29c5a7dad49d054f96669e4943e14ba066847
Anyudat <132820907+Anyudat@users.noreply.github.com>
2023-11-02 17:29:29 +0100

    Add files via upload

--- elemek.js
+++ elemek.js
@@ -1,1 +1,16 @@
 export const FEJ = "url('./kepek/snake.png')";
+export const FEJ_JOBBRA = "url('./kigyo_grafika/snake_head_right_no_tongue.png')";
+export const FEJ_LE = "url('./kigyo_grafika/snake_head_down_no_tongue.png')";
+export const FEJ_BALRA = "url('./kigyo_grafika/snake_head_left_no_tongue.png')";
+export const FEJ_FEL = "url('./kigyo_grafika/snake_head_up_no_tongue.png')";
+export const TEST_VIZSZINTES = "url('./kigyo_grafika/body_left.png')";
+export const TEST_FUGGOLEGES = "url('./kigyo_grafika/body_down.png')";
+export const TEST_SAROK1 = "url('./kigyo_grafika/body_down_left.png')";
+export const TEST_SAROK2 = "url('./kigyo_grafika/body_down_right2.png')";
+export const TEST_SAROK3 = "url('./kigyo_grafika/body_up_left2.png')";
+export const TEST_SAROK4 = "url('./kigyo_grafika/body_up_right.png')";
+export const CSERESZNYE = "url('./kigyo_grafika/cherry.png')";
+export const FAROK_JOBBRA = "url('./kigyo_grafika/tail_right.png')";
+export const FAROK_BALRA = "url('./kigyo_grafika/tail_left.png')";
+export const FAROK_FEL = "url('./kigyo_grafika/tail_up.png')";
+export const FAROK_LE = "url('./kigyo_grafika/tail_down.png')";
--- kigyo.js
+++ kigyo.js
@@ -4,21 +4,25 @@ let KIGYOFEJ = document.querySelector("#kigyofej");
 KIGYOFEJ.irany = "ArrowRight";
 const JATEKMEZO = document.querySelector("#jatekmezo");
 const VEGE = document.querySelector("#vege");
+const pont = document.querySelector("#score");
 const ELEMNAGYSAG = 50;
 const PALYASZELESSEG = JATEKMEZO.clientWidth;
 const PALYAMAGASSAG = JATEKMEZO.clientHeight;
 const alma = document.createElement("div");
+const SEBESSEG = 130;
 let fut = false;
 let kigyo = [];
 let el = false;
 let pontok = 0;
 let elkezdodott = false;
 let helyek = [];
+let foroghat = true;
 
 kigyotest();
 
 // kigyótest
 function kigyotest(){
+    pont.textContent = `pontok: ${pontok}`;
     while(JATEKMEZO.firstChild){
         JATEKMEZO.removeChild(JATEKMEZO.firstChild);
     }
@@ -29,7 +33,7 @@ function kigyotest(){
     KIGYOFEJ.x = 3*ELEMNAGYSAG;
     KIGYOFEJ.y = 0;
     KIGYOFEJ.irany = "ArrowRight";
-    KIGYOFEJ.style.backgroundImage = elemek.FEJ;
+    KIGYOFEJ.style.backgroundImage = elemek.FEJ_JOBBRA;
     KIGYOFEJ.style.left = KIGYOFEJ.x + "px";
     KIGYOFEJ.style.top = KIGYOFEJ.y + "px";
     kigyo.push(KIGYOFEJ);
@@ -37,7 +41,7 @@ function kigyotest(){
         kigyo.push(document.createElement("div"));
         kigyo[i].className = "testresz";
         JATEKMEZO.appendChild(kigyo[i]);
-        kigyo[i].style.backgroundImage = elemek.FEJ;
+        kigyo[i].style.backgroundImage = elemek.TEST_VIZSZINTES;
         kigyo[i].x = kigyo[0].x - ELEMNAGYSAG*i;
         kigyo[i].y = kigyo[0].y - ELEMNAGYSAG*i;
         kigyo[i].style.position = "relative";
@@ -48,6 +52,7 @@ function kigyotest(){
         kigyo[i].style.top = kigyo[i].y + "px";
         kigyo[i].irany = "ArrowRight";
     }
+    farok();
     random_alma();
     JATEKMEZO.append(alma);
 }
@@ -85,25 +90,31 @@ function kovi(){
                 rajzol();
             }
             kovi();
-        }, 100)
+        }, SEBESSEG);
     }
 }
 
-function iranyvaltozas(event){
-    if (KIGYOFEJ.irany == "ArrowUp" && event.key != "ArrowDown"){
-        KIGYOFEJ.irany = event.key;
-    }
-    if (KIGYOFEJ.irany == "ArrowDown" && event.key != "ArrowUp"){
-        KIGYOFEJ.irany = event.key;
-    }
-    if (KIGYOFEJ.irany == "ArrowLeft" && event.key != "ArrowRight"){
-        KIGYOFEJ.irany = event.key;
-    }
-    if (KIGYOFEJ.irany == "ArrowRight" && event.key != "ArrowLeft"){
-        KIGYOFEJ.irany = event.key;
+const varj = () => new Promise(res => setTimeout(res, SEBESSEG));
+
+const iranyvaltozas = async (event) => {
+    if (foroghat){
+        foroghat = false;
+        if (KIGYOFEJ.irany == "ArrowUp" && event.key != "ArrowDown") {
+            KIGYOFEJ.irany = event.key;
+        }
+        if (KIGYOFEJ.irany == "ArrowDown" && event.key != "ArrowUp") {
+            KIGYOFEJ.irany = event.key;
+        }
+        if (KIGYOFEJ.irany == "ArrowLeft" && event.key != "ArrowRight") {
+            KIGYOFEJ.irany = event.key;
+        }
+        if (KIGYOFEJ.irany == "ArrowRight" && event.key != "ArrowLeft") {
+            KIGYOFEJ.irany = event.key;
+        }
+        await varj();
+        foroghat = true;
     }
 }
-
 function mozog(){
     for(let j = 0; j < kigyo.length; j++){
         if (j != 0){
@@ -139,7 +150,62 @@ function mozog(){
 
 function rajzol(){
     for(let l = 0; l < kigyo.length; l++){
-        if(l < 4){
+        if (l == 0){
+            kigyo[l].style.left = kigyo[l].x + "px";
+            kigyo[l].style.top = kigyo[l].y + "px";
+                if (kigyo[l].irany == "ArrowDown"){
+                    kigyo[l].style.backgroundImage = elemek.FEJ_LE;
+                }
+                if (kigyo[l].irany == "ArrowLeft"){
+                    kigyo[l].style.backgroundImage = elemek.FEJ_BALRA;
+                }
+                if (kigyo[l].irany == "ArrowUp"){
+                    kigyo[l].style.backgroundImage = elemek.FEJ_FEL;
+                }
+                if (kigyo[l].irany == "ArrowRight"){
+                    kigyo[l].style.backgroundImage = elemek.FEJ_JOBBRA;
+                }
+            }
+        if (l > 0 && l < kigyo.length-1){
+            if (kigyo[l - 1].irany == "ArrowDown" && kigyo[l].irany == "ArrowRight"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK3;
+            }
+            if (kigyo[l - 1].irany == "ArrowDown" && kigyo[l].irany == "ArrowLeft"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK1;
+            }
+            if (kigyo[l - 1].irany == "ArrowDown" && kigyo[l].irany == "ArrowDown"){
+                kigyo[l].style.backgroundImage = elemek.TEST_FUGGOLEGES;
+            }
+            if (kigyo[l - 1].irany == "ArrowUp" && kigyo[l].irany == "ArrowRight"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK4;
+            }
+            if (kigyo[l - 1].irany == "ArrowUp" && kigyo[l].irany == "ArrowLeft"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK2;
+            }
+            if (kigyo[l - 1].irany == "ArrowUp" && kigyo[l].irany == "ArrowUp"){
+                kigyo[l].style.backgroundImage = elemek.TEST_FUGGOLEGES;
+            }
+            if (kigyo[l - 1].irany == "ArrowRight" && kigyo[l].irany == "ArrowUp"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK1;
+            }
+            if (kigyo[l - 1].irany == "ArrowRight" && kigyo[l].irany == "ArrowDown"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK2;
+            }
+            if (kigyo[l - 1].irany == "ArrowRight" && kigyo[l].irany == "ArrowRight"){
+                kigyo[l].style.backgroundImage = elemek.TEST_VIZSZINTES;
+            }
+            if (kigyo[l - 1].irany == "ArrowLeft" && kigyo[l].irany == "ArrowUp"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK3;
+            }
+            if (kigyo[l - 1].irany == "ArrowLeft" && kigyo[l].irany == "ArrowDown"){
+                kigyo[l].style.backgroundImage = elemek.TEST_SAROK4;
+            }
+            if (kigyo[l - 1].irany == "ArrowLeft" && kigyo[l].irany == "ArrowLeft"){
+                kigyo[l].style.backgroundImage = elemek.TEST_VIZSZINTES;
+            }
+            
+        }
+        if (l < 4){
             kigyo[l].style.left = kigyo[l].x + "px";
             kigyo[l].style.top = kigyo[l].y + "px";
         }
@@ -148,6 +214,7 @@ function rajzol(){
             kigyo[l].style.top = (kigyo[l].y - ELEMNAGYSAG) + "px";
         }
     }
+    farok();
     
 }
 
@@ -167,9 +234,11 @@ function halal(){
 function eszik(){
     if (parseInt(KIGYOFEJ.x / ELEMNAGYSAG) == alma.x && parseInt(KIGYOFEJ.y / ELEMNAGYSAG) == (alma.y + 4)){
         pontok += 1;
+        pont.textContent = `pontok: ${pontok}`;
         random_alma();
         kigyo.push(document.createElement("div"));
         div_tulajdonsagok(kigyo[kigyo.length - 1]);
+        farok();
     }
 }
 
@@ -184,24 +253,27 @@ function random_alma(){
     helyek.forEach(koordinata => {
         for (let q = 0; q < kigyo.length; q++) {
             koordinata.forEach(hely => {
-                console.log(q);
-                console.log(kigyo[q].y);
-                console.log(hely[1]);
                 if (kigyo[q].x / ELEMNAGYSAG == hely[1] && (kigyo[q].y / ELEMNAGYSAG + q) + 4 == hely[0]) {
                     koordinata.splice(koordinata.indexOf(hely), 1);
                 }
             });    
         }
     });
-    console.log(helyek);
+    helyek.forEach(koordinata => {
+        if (koordinata == []){
+            helyek.splice(helyek.indexOf(koordinata),1);
+        }
+    })
     let oszlop = helyek[parseInt(Math.floor(Math.random() * helyek.length))]; 
     let sor = oszlop[Math.floor(Math.random()*oszlop.length)];
+    console.log(helyek);
+    console.log(oszlop);
     console.log(sor);
     alma.x = sor[1];
     alma.y = sor[0] - 8;
     alma.style.left = (alma.x * ELEMNAGYSAG) + "px";
     alma.style.top = (alma.y * ELEMNAGYSAG) + "px";
-    alma.style.backgroundImage = "url('./kepek/amoba.jpg')";
+    alma.style.backgroundImage = elemek.CSERESZNYE;
     alma.style.position = "relative";
     alma.style.width = ELEMNAGYSAG + "px";
     alma.style.height = ELEMNAGYSAG + "px";
@@ -214,10 +286,24 @@ function div_tulajdonsagok(div){
     div.y = kigyo[kigyo.length - 2].y - ELEMNAGYSAG;
     div.style.left = div.x + "px";
     div.style.top = div.y + "px";
-    div.style.backgroundImage = elemek.FEJ;
     div.style.backgroundSize = "contain";
     div.style.width = ELEMNAGYSAG + "px";
     div.style.height = ELEMNAGYSAG + "px";
     div.style.position = "relative";
     JATEKMEZO.appendChild(kigyo[kigyo.length - 1]);
+}
+
+function farok(){
+    if (kigyo[kigyo.length - 2].irany == "ArrowDown"){
+        kigyo[kigyo.length - 1].style.backgroundImage = elemek.FAROK_LE;
+    }
+    if (kigyo[kigyo.length - 2].irany == "ArrowUp") {
+        kigyo[kigyo.length - 1].style.backgroundImage = elemek.FAROK_FEL;
+    }
+    if (kigyo[kigyo.length - 2].irany == "ArrowLeft") {
+        kigyo[kigyo.length - 1].style.backgroundImage = elemek.FAROK_BALRA;
+    }
+    if (kigyo[kigyo.length - 2].irany == "ArrowRight") {
+        kigyo[kigyo.length - 1].style.backgroundImage = elemek.FAROK_JOBBRA;
+    }
 }
